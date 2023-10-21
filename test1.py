4d5a8148f57fb5825cc4de3616932f6c337118d9
mvtply <83705509+mvtply@users.noreply.github.com>
2023-10-21 12:50:14 +0200

    fixed api leak

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 import os
 import openai
 openai.organization = "org-HqxhElREZ1F4BKsfH5w4P8IO"
-openai.api_key = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 x = openai.Model.list()
 print(x)
