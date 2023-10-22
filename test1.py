7520d3bc72e00dab6e6ab238e3b7b02fc4c431b6
mvtply <83705509+mvtply@users.noreply.github.com>
2023-10-22 16:13:53 +0200

    refractored functions to custfunct

--- test1.py
+++ test1.py
@@ -1,30 +1,20 @@
 import os
 import openai
-import tiktoken
 import json
+from custfunct import num_tokens_from_messages
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 model = "gpt-3.5-turbo"
-encoding = tiktoken.encoding_for_model(model)
-
-def num_tokens_from_string(string: str, model_name: str) -> int:
-    """Returns the number of tokens in a text string."""
-    encoding = tiktoken.encoding_for_model(model_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-
 messages = [
     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
   ]
-print(num_tokens_from_string(json.dumps(messages), model))
+  
+print(f"{num_tokens_from_messages(messages, model)} prompt tokens counted by num_tokens_from_messages().")
 
-'''
-completion = openai.ChatCompletion.create(
+response = openai.ChatCompletion.create(
   model=model,
   messages=messages
 )
-
-print(completion.choices[0].message)
-'''
+print(response.choices[0].message)
+print(f'{response["usage"]["prompt_tokens"]} prompt tokens counted by the OpenAI API.')
